6a2cda4ab73d7147e3c6f05878ab89092deadb4a
Ernie <emetcalf44@gmail.com>
2021-03-19 17:30:08 +0000

    question 5 completed

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,17 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+
+const time = distance / speed;
+const willArrive = Math.round(time * 2)/2;
+
+    if (willArrive <= 0.5) {
+        return `I should be there in half an hour`
+    } else if (willArrive === 1) {
+        return `I should be there in an hour`
+    } else {
+    return `I should be there in ${willArrive} hours`
+    }
+
+};
+
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,21 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toEqual(`I should be there in 4.5 hours`)
+    expect(reachDestination(60, 12)).toEqual(`I should be there in 5 hours`)
+    expect(reachDestination(70, 6)).toEqual(`I should be there in 11.5 hours`)
+  });
+
+  it("returns string with estimated time of arrival is 1 hour", () => {
+    expect(reachDestination(10, 9)).toEqual(`I should be there in an hour`)
+    expect(reachDestination(8, 8)).toEqual(`I should be there in an hour`)
+  });
+
+  it("returns string with estimated time of arrival is half an hour", () => {
+    expect(reachDestination(2, 4)).toEqual(`I should be there in half an hour`)
+    expect(reachDestination(10, 25)).toEqual(`I should be there in half an hour`)
+  });
+
+  
 });
